0ab2e52af82d573478733ba8ec47cfc4611a0294
Jeremy IGONIN <jeigonin@iutclinfa1011m.local>
2018-02-02 08:06:58 +0100

    Initial Commit

---
+++ NavyBattle/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NavyBattle
+//
+//  Created by Jeremy IGONIN on 02/02/2018.
+//  Copyright © 2018 Jeremy IGONIN. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
